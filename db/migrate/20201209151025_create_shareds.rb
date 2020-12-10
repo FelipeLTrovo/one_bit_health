8852ee066f751134c2bf05de440e53cfbc4a954a
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2020-12-10 09:26:48 -0300

    Sets up shared model to user references

--- db/migrate/20201209151025_create_shareds.rb
+++ db/migrate/20201209151025_create_shareds.rb
@@ -1,10 +1,10 @@
 class CreateShareds < ActiveRecord::Migration[6.0]
   def change
     create_table :shareds do |t|
-      t.user :references
+      t.references :user, null: false, foreign_key: true
       t.belongs_to :professional, references: :user
 
-      t.references :sharedable, polymorphic: true, null: false  
+      t.references :sharedable, polymorphic: true, null: false
 
       t.timestamps
     end
