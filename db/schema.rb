00b378f95a72fdb2836c74101bbca87ea7a492e9
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2020-12-10 15:54:55 -0300

    Updates schema for tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_165715) do
+ActiveRecord::Schema.define(version: 2020_12_10_175128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tags", force: :cascade do |t|
+    t.string "content"
+    t.string "tagable_type", null: false
+    t.bigint "tagable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tagable_type", "tagable_id"], name: "index_tags_on_tagable_type_and_tagable_id"
+  end
 
   create_table "treatments", force: :cascade do |t|
     t.string "title"
@@ -23,7 +32,6 @@ ActiveRecord::Schema.define(version: 2020_12_10_165715) do
     t.integer "kind"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["tagable_type", "tagable_id"], name: "index_tags_on_tagable_type_and_tagable_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,5 +48,4 @@ ActiveRecord::Schema.define(version: 2020_12_10_165715) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "shareds", "users"
 end
