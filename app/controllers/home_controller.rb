54d26a128ed0d551b3fd295d20c626f08529cdc4
cesar <cesar@coreplan.com.br>
2020-12-12 15:03:22 -0300

    Pagina inicial e pesquisa de consultas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:about]
 
   def index
+    @appointments = Appointment.order(:created_at).limit(3).reverse()
+    @exams = Exam.order(:created_at).limit(3).reverse()
+    @treatments = Treatment.order(:created_at).limit(3).reverse()
   end
 
   def about
