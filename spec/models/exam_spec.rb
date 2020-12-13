a8f7645183cec03c702cd675203db313a5b9add2
Fabio Vilhena <fvilhena@gmail.com>
2020-12-13 14:24:40 -0300

    Exams test

--- spec/models/exam_spec.rb
+++ spec/models/exam_spec.rb
@@ -16,6 +16,7 @@
 require 'rails_helper'
 
 RSpec.describe Exam, type: :model do
+  it { is_expected.to belong_to :user }
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:exam_date) }
